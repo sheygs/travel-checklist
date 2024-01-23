9d80410b4ff4586c4f0a7d4de69bc2ad2847d070
Olusegun Ekoh @sheygs13 <segun.ekoh@gmail.com>
2024-01-23 10:49:46 +0100

    feat: Flesh out components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+import ListItem from './components/ListItem';
+import Statistics from './components/Statistic';
 
 function App() {
         return (
-                <div className="App">
-                        <header className="App-header">Travel Checklist</header>
+                <div>
+                        {/* <header className="App-header">Travel Checklist</header> */}
+
+                        <Header />
+                        <Form />
+                        <ListItem />
+                        <Statistics />
                 </div>
         );
 }
